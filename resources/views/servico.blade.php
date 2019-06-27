52629766a351ad009b057e2ffd74199a55308317
Luiz Carlos Efigênio da Rosa Junior <hanzou0012@gmail.com>
2019-06-27 12:34:24 -0300

    organizando serviço

--- resources/views/servico.blade.php
+++ resources/views/servico.blade.php
@@ -9,6 +9,34 @@
 
 @section('conteudo')
 
-<h3> CONTEÚDO PÁGINA DOS SERVIÇOS </h3>
-
+<h3> SERVIÇOS </h3>
+<div class='row'>
+	<div class='col-sm-8' style="text-align: center">
+		<a  type="button" class="btn btn-primary btn-block">
+		<b>Cadastrar Novo Serviço</b>
+		</a>
+	</div>
+	<div class='col-sm-1' style="text-align: center">
+        <button type="button" class="btn btn-default btn-block">
+            <span class="glyphicon glyphicon-search"></span>
+        </button>
+    </div>
+	 <br>
+	<!-- <table class='table table-striped'>
+   	<thead>
+        <tr>
+            <th>ID</th>
+            <th>SERVIÇO</th>
+            <th>VALOR</th>
+            <th>TEMPO DE SERVIÇO</th>
+        </tr>
+    </thead>
+    <tbody>
+	 <tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td>
+	 </tr> -->
+</div>
 @stop
